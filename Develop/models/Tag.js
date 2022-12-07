0968d114e040a645f0e27a9f2faf9bd75462d56b
EdenABE <eden.a.biltibo@vumc.org>
2022-12-07 01:01:18 -0500

    Readme

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require("../config/connection.js");
+const sequelize = require("../Config/connection.js");
 
 class Tag extends Model {}
 
